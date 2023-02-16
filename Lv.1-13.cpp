0eaf62ce27bfc2bebd1086835ce1dcb2a37d10e3
100SongH <thdgml322@seoultech.ac.kr>
2023-02-16 23:29:33 +0900

    과일 장수

---
+++ Lv.1-13.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool compare(int a, int b) {
+    return a > b;
+}
+int solution(int k, int m, vector<int> score) {
+    int answer = 0;
+    sort(score.begin(), score.end(), compare);
+
+    for (int i = 0; i < score.size(); i++) {
+        if ((i + 1) % m == 0) {
+            answer += score[i] * m;
+        }
+    }
+    return answer;
+}
