4157886a216d062b133a303eff0053a4ec933d97
백송희 <88391162+1mSSong@users.noreply.github.com>
2024-10-25 17:04:06 +0900

    균형잡힌 세상

---
+++ Q4949.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main() {
+	string str;
+
+	while (1) {
+
+		getline(cin, str);
+
+		stack<char> s;
+
+		if (str == ".")
+			break;
+
+
+		int len = str.length();
+
+		int flag = 0;
+
+		for (int i = 0; i < len; ++i) {
+			if (str[i] == '(') {
+				s.push('(');
+			}
+
+			else if (str[i] == ')') {
+				if (s.empty()) {
+					flag = 1;
+					break;
+				}
+
+				if (s.top() == '(')
+					s.pop();
+				else {
+					flag = 1;
+					break;
+				}
+			}
+
+			else if (str[i] == '[')
+				s.push('[');
+
+			else if (str[i] == ']') {
+				if (s.empty()) {
+					flag = 1;
+					break;
+				}
+
+				if (s.top() == '[') {
+					s.pop();
+				}
+				else {
+					flag = 1;
+					break;
+				}
+			}
+		}
+		
+		if (s.size() != 0)
+			flag = 1;
+
+		(flag == 0) ? cout << "yes\n" : cout << "no\n";
+
+	}
+
+}
