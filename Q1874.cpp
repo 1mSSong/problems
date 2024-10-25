79314e50192597dc473be6ac77005af3b37570c5
백송희 <88391162+1mSSong@users.noreply.github.com>
2024-10-25 17:03:39 +0900

    스택 수열

---
+++ Q1874.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+stack<int> s;
+vector<int> arr;
+vector<char> answer;
+
+int n;
+
+int index;
+
+void input() {
+	cin >> n;
+	
+	arr.resize(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> arr[i];
+	}
+}
+
+bool solution() {
+
+	for (int i = 1; i <= n; ++i) {
+		answer.push_back('+');
+		s.push(i);
+
+		while (1) {
+			if (index >= n)
+				break;
+
+			if (s.empty())
+				break;
+
+			if (s.top() != arr[index])
+				break;
+
+			s.pop();
+			answer.push_back('-');
+			index++;
+		}
+	}
+
+	return s.empty();
+}
+
+int main() {
+	input();
+
+
+	if (solution()) {
+		for (int i = 0; i < answer.size(); ++i) {
+			cout << answer[i] << "\n";
+		}
+	}
+	else {
+		cout << "NO";
+	}
+	 
+}
